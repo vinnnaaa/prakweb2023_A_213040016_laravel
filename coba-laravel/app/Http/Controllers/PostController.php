2f599e74eb96d08ed7efcb0c342ea68d161c61f8
vinnnaaa <bellarevina23@gmail.com>
2023-11-05 01:20:10 +0700

    Vidio 7

--- coba-laravel/app/Http/Controllers/PostController.php
+++ coba-laravel/app/Http/Controllers/PostController.php
@@ -16,11 +16,11 @@ class PostController extends Controller
     ]);
     }
 
-    public function show($slug) 
+    public function show($id) 
     {
         return view('post', [
             "title" => "Single Post",
-            "post" => Post::find($slug)
+            "post" => Post::find($id)
         ]);
     }
 }
---
+++ coba-laravel/resources/views/post.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.main')
+
+@section('container')
+<article>
+    <h2>{{ $post->title }}</h2>
+    <h5>{{ $post->Author }}</h5>
+    {{ $post["body"] }}
+</article>
+    <a href="/blog">Back to Posts</a>
+    
+@endsection
