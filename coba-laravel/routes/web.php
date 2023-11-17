3310afe59a8105722d148b4dba53c394c339045e
vinnnaaa <bellarevina23@gmail.com>
2023-11-18 01:12:54 +0700

    Vidio 17

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,13 +1,12 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
 use App\Models\Category;
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
-use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
+use App\Http\Controllers\DashboardPostController;
 
 
 /*
@@ -59,4 +58,10 @@ Route::get('/register', [RegisterController::class, 'index'])->middleware('guest
 
 Route::post('/register', [RegisterController::class, 'store']);
 
-Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard', function() {
+    return view('dashboard.index');
+})->middleware('auth');
+
+
+Route::resource('/dashboard/posts', DashboardPostController::class)
+->middleware('auth');
