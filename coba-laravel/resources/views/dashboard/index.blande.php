02d8f0a49d49f337753c00558b988fa2ad110a3d
vinnnaaa <bellarevina23@gmail.com>
2023-11-18 00:58:19 +0700

    Vidio 16

---
+++ coba-laravel/resources/views/dashboard/index.blande.php
@@ -0,0 +1,1 @@
+<h1>Welcome, Revina Bella Trisna</h1>
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,9 +1,13 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
+use App\Models\Category;
+
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use SebastianBergmann\CodeCoverage\Report\Html\Dashboard;
 
 
 /*
@@ -47,8 +51,12 @@ Route::get('/categories', function() {
     ]);
 });
 
-Route::get('/login', [LoginController::class, 'index']);
+Route::get('/login', [LoginController::class, 'index'])->name('login')->middleware('guest');
+Route::post('/login', [LoginController::class, 'authenticate']);
+Route::post('/logout', [LoginController::class, 'logout']);
 
-Route::get('/register', [RegisterController::class, 'index']);
+Route::get('/register', [RegisterController::class, 'index'])->middleware('guest');
 
 Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/dashboard', [DashboardController::class, 'index'])->middleware('auth');
