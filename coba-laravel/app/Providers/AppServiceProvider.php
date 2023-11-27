d22d03e00d8e5d43dff384d091b3695e85da02b3
vinnnaaa <bellarevina23@gmail.com>
2023-11-27 23:15:16 +0700

    Vidio 23

--- coba-laravel/app/Providers/AppServiceProvider.php
+++ coba-laravel/app/Providers/AppServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Pagination\Paginator;
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,6 +23,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        Schema::defaultStringLength(191);
         Paginator::useBootstrap();
+        Gate::define('admin', function(User $user) {
+            return $user->is_admin;
+         });
     }
 }
--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminCategoryController;
 use App\Models\Category;
 
 use Illuminate\Support\Facades\Route;
@@ -69,3 +70,6 @@ Route::get('/dashboard/posts/checkSlug', [DashboardPostController::class,'checkS
 
 Route::resource('/dashboard/posts', DashboardPostController::class)
 ->middleware('auth');
+
+Route::resource('/dashboard/categories', AdminCategoryController::class)->except('show')
+->middleware('admin');
